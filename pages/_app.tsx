70600999a355f4ce737148914194b6ec546501ff
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-04 10:01:22 -0600

    feat: import and use codemirror with github markdown styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { AppProps } from 'next/app'
 
 import '../styles/index.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/base16-light.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,66 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+import { Controlled as CodeMirror } from 'react-codemirror2'
+
+/* 
+  dynamically import codemirror only on client
+  since it looks for 'navigator' to render which
+  is only in the browser
+*/
+dynamic(() => {
+  return import('codemirror/mode/gfm/gfm')
+}, { ssr: false })
+
+// from https://github.com/taniarascia/takenote/blob/master/src/client/slices/settings.ts
+const codeMirrorOptions = {
+  mode: 'gfm',
+  theme: 'base16-light',
+  lineNumbers: false,
+  lineWrapping: true,
+  viewportMargin: Infinity,
+  keyMap: 'default',
+  dragDrop: false,
+}
+
+// from https://rexxars.github.io/react-markdown/
+const initialState = `# Live demo
+
+Changes are automatically rendered as you type.
+## Table of Contents
+* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
+* Renders actual, "native" React DOM elements
+* Allows you to escape or skip HTML (try toggling the checkboxes above)
+* If you escape or skip the HTML, no \`dangerouslySetInnerHTML\` is used! Yay!
+## HTML block below
+<blockquote>
+  This blockquote will change based on the HTML settings above.
+</blockquote>
+## How about some code?
+\`\`\`js
+var React = require('react');
+var Markdown = require('react-markdown');
+React.render(
+  <Markdown source="# Your markdown here" />,
+  document.getElementById('content')
+);
+\`\`\`
+Pretty neat, eh?
+## Tables?
+| Feature   | Support |
+| --------- | ------- |
+| tables    | ✔ |
+| alignment | ✔ |
+| wewt      | ✔ |
+## More info?
+Read usage information and more on [GitHub](//github.com/rexxars/react-markdown)
+---------------
+A component by [Espen Hovlandsdal](https://espen.codes/)
+`
 
 const IndexPage: NextPage = () => {
+  const [text, setText] = useState(initialState)
   const formatDate = () => {
     const today = new Date()
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
@@ -21,7 +79,19 @@ const IndexPage: NextPage = () => {
             <h1 className="mb-2 font-bold text-2xl text-green-500 md:text-3xl md:mb-0">{formatDate()}</h1>
             <button className="px-4 py-1 text-green-500 border-green-500 border rounded-lg font-semibold hover:text-white hover:bg-green-400">Preview</button>
           </div>
-          <textarea className="w-full md:text-lg" rows={20} placeholder="Write more here" />
+          <CodeMirror
+            options={codeMirrorOptions}
+            value={text}
+            editorDidMount={editor => {
+              setTimeout(() => {
+                editor.focus()
+              }, 0)
+              editor.setCursor(0)
+            }}
+            onBeforeChange={(_editor, _data, value) => {
+              setText(value)
+            }}
+          />
         </main>
         <footer className="flex justify-end">Word Count: 0</footer>
       </div>
