5f94b108f399c131694e40dfd13661801182db4b
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-04 12:07:13 -0600

    refactor: add list inside styling

--- components/MarkdownRenderers/ListRenderer.tsx
+++ components/MarkdownRenderers/ListRenderer.tsx
@@ -15,7 +15,7 @@ const ListRenderer: React.FC<ListProps> = ({ children, ordered }) => {
     <Ul>{children}</Ul>
 }
 
-const Ol: React.FC<Props> = ({ children }) => <ol className="list-decimal mb-6">{children}</ol>
-const Ul: React.FC<Props> = ({ children }) => <ul className="list-disc mb-6">{children}</ul>
+const Ol: React.FC<Props> = ({ children }) => <ol className="list-decimal list-inside mb-6">{children}</ol>
+const Ul: React.FC<Props> = ({ children }) => <ul className="list-disc list-inside mb-6">{children}</ul>
 
 export default ListRenderer
