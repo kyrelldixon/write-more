56e104714dac52732542a4abe184af518560462d
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-04 12:05:28 -0600

    feat: add hr renderer

---
+++ components/MarkdownRenderers/HorizontalRuleRenderer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HorizontalRuleRenderer: React.FC = () => {
+  return (
+    <hr className="mb-6" />
+  )
+}
+
+export default HorizontalRuleRenderer
--- components/MarkdownRenderers/index.tsx
+++ components/MarkdownRenderers/index.tsx
@@ -2,10 +2,12 @@ import HeadingRenderer from './HeadingRenderer'
 import ListRenderer from './ListRenderer'
 import LinkRenderer from './LinkRenderer'
 import ParagraphRenderer from './ParagraphRender'
+import HorizontalRuleRenderer from './HorizontalRuleRenderer'
 
 export default {
   heading: HeadingRenderer,
   list: ListRenderer,
   paragraph: ParagraphRenderer,
   link: LinkRenderer,
+  thematicBreak: HorizontalRuleRenderer,
 }
