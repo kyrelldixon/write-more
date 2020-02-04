63b9e7b7bf0b6f8e988b1ac194b014d9d49c5adf
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-04 11:58:57 -0600

    feat: add link renderer

---
+++ components/MarkdownRenderers/LinkRenderer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type LinkProps = {
+  children: React.ReactNode
+  href: string
+}
+
+const ParagraphRenderer: React.FC<LinkProps> = ({ children, href }) => {
+  return (
+    <a
+      href={href}
+      className="text-blue-500"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  )
+}
+
+export default ParagraphRenderer
--- components/MarkdownRenderers/index.tsx
+++ components/MarkdownRenderers/index.tsx
@@ -1,9 +1,11 @@
 import HeadingRenderer from './HeadingRenderer'
 import ListRenderer from './ListRenderer'
+import LinkRenderer from './LinkRenderer'
 import ParagraphRenderer from './ParagraphRender'
 
 export default {
   heading: HeadingRenderer,
   list: ListRenderer,
   paragraph: ParagraphRenderer,
+  link: LinkRenderer,
 }
